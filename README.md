454a17814d3dfab777302986287918675db66a68
julsas <julian.sass@bihealth.de>
2023-04-05 18:44:10 +0200

    Added script and readme

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # ig-export-rename
+
+A script to modify the .html files exported from a Simplifier implementation guide.
+The tool is primarily intended to quickly generate a desired url scheme.
+
+## Prerequisites
+* Python 3.7 or later
+
+## Usage
+* Export an implementation guide from Simplifier and receive the zip file
+* In `ig-rename.py` modify the `path_to_zip_file` variable to point to the zip file
+* Define replacements for unwanted substrings in file names and html body in the `replacements` dict
+* Run:
+
+```bash
+python ig-rename.py
+```
---
+++ ig-rename.py
@@ -0,0 +1,57 @@
+import os
+import re
+import shutil
+import zipfile
+
+# Define the path to the zip file
+path_to_zip_file = "modul-medikation-ig-1.0-de@current.zip"
+
+# Define the directory containing the HTML files
+#directory = "modul-medikation-ig-1.0-de@current"
+directory = path_to_zip_file.replace(".zip", "")
+
+# Define the directory to extract to
+with zipfile.ZipFile(path_to_zip_file, 'r') as zip_ref:
+    zip_ref.extractall(directory)
+
+# Define a function to replace multiple substrings in a string
+def multi_replace_regex(string, replacements, ignore_case=False):
+    for pattern, repl in replacements.items():
+        string = re.sub(pattern, repl, string, flags=re.I if ignore_case else 0)
+    return string
+
+# Define replacements for unwanted substrings in file names and html body
+# Python 3.7 or later needed to preserve the order of the replacements in the dictionary
+replacements = {
+    'ImplementationGuide-1.x-TechnischeImplementierung-': '',
+    'ImplementationGuide-1.x/TechnischeImplementierung/': '',
+    'ImplementationGuide-2.x-TechnischeImplementierung-': '',
+    'ImplementationGuide-2.x/TechnischeImplementierung/': '',
+    'ImplementationGuide-1.x-': '',
+    'ImplementationGuide-1.x/': '',
+    'ImplementationGuide-2.x-': '',
+    'ImplementationGuide-2.x/': ''
+    }
+
+# Loop through each file in the directory
+for filename in os.listdir(directory):
+
+    # Check if the file is an HTML file
+    if filename.endswith(".html"):
+
+        # Construct the old and new file paths
+        old_path = os.path.join(directory, filename)
+        new_path = os.path.join(directory, f"{multi_replace_regex(filename, replacements)}")
+
+        # Rename the file
+        os.rename(old_path, new_path)
+
+        # Update the links in the HTML file
+        with open(new_path, "r+") as f:
+            content = f.read()
+            f.seek(0)
+            f.truncate()
+            f.write(multi_replace_regex(content, replacements))
+
+# Create zip file
+shutil.make_archive(directory, 'zip', directory)
